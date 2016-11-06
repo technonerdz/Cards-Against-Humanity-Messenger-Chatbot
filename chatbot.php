9c3eb42c111600e3fab80323f67a9ad33aca39cb
technonerdz <technonerdzfg@gmail.com>
2016-11-06 10:51:38 -0500

    Init chatbot.php

---
+++ chatbot.php
@@ -0,0 +1,35 @@
+<?php
+// parameters
+$accessToken = "xxx";
+$hubVerifyToken = 'xxx';
+// check token at setup
+if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+  echo $_REQUEST['hub_challenge'];
+  exit;
+}
+// handle bot's anwser
+$input = json_decode(file_get_contents('php://input'), true);
+$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+
+$answer = "I don't understand. Type 'Play' to get started.";
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
+
+if($messageText == "hi") {
+    $answer = "Hello! Type 'Play' to get started.";
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
+
+}
+
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_exec($ch);
+curl_close($ch);
